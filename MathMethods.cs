a0016ec636ac9c52d14928a2da0c9582152473a4
Autif Kamal <kautif@gmail.com>
2023-03-29 12:08:50 -0500

    added write and read lines

---
+++ MathMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class MathMethods
+{
+	public MathMethods()
+	{
+
+	}
+
+	public static int Add (params int[] numbers) {
+		int sum = 0;
+		
+		foreach (int n in numbers)
+		{
+			sum += n;
+		}
+
+		Console.WriteLine("Sum: ", sum);
+	}
+
+	Add(1, 2, 3);
+}
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite Band?");
+            var band = Console.ReadLine();
+
+            Console.WriteLine($"Here's a story about {name}. They bought a {color} car with a giant {animal} sticker plastered on the side and they took {band} for a ride in their car");
         }
     }
 }
