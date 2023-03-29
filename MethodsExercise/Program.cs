dcb62caaceb318572f8a362f1586959589c4d604
Autif Kamal <kautif@gmail.com>
2023-03-29 12:17:30 -0500

    added multiply and add methods

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -17,6 +17,40 @@
             var band = Console.ReadLine();
 
             Console.WriteLine($"Here's a story about {name}. They bought a {color} car with a giant {animal} sticker plastered on the side and they took {band} for a ride in their car");
+
+            var sum = Add(1, 2, 3, 4, 5);
+
+            Console.WriteLine(sum);
+
+            var multiples = Multiply(1, 2, 3, 4, 5);
+            Console.WriteLine(multiples);
+
+        }
+
+        public static int Add(params int[] numbers)
+        {
+            int sum = 0;
+
+            foreach (int n in numbers)
+            {
+                sum += n;
+            }
+
+            Console.WriteLine("Sum: ", sum);
+            return sum;
+        }
+
+        public static int Multiply(params int[] numbers)
+        {
+            int product = 1;
+
+            foreach (int n in numbers)
+            {
+                product *= n;
+            }
+
+            Console.WriteLine("Product: ", product);
+            return product;
         }
     }
 }
